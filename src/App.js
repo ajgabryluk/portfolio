e9870cb0d3213a00985052652fd3f81bec047b1e
DrFeelGoodz <aarong1@ptd.net>
2023-12-15 14:29:17 -0500

    Created fototer, header, and about components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
-import Home from './Components/Home';
+import Home from "./Components/Home"
 import Projects from './Components/Projects';
+import About from './Components/About';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
-      <header>
-        <h1>THIS IS THE HEADER</h1>
-      </header>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/projects" element={<Projects />}/>
+        <Route path="/about" element={<About />}/>
       </Routes>
-      <footer>
-        <h1>THIS IS THE FOOTER</h1>
-      </footer>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <h1>tHIS IS THE HEADER</h1>
+        </header>
+    )
+}
