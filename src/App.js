036d7468fbb5354019698188493d8a7d16d04829
DrFeelGoodz <aarong1@ptd.net>
2023-12-15 13:40:53 -0500

    Created routes for portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Home from './Components/home';
+import Projects from './Components/projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>THIS IS THE HEADER</h1>
       </header>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/projects" element={<Projects />}/>
+      </Routes>
+      <footer>
+        <h1>THIS IS THE FOOTER</h1>
+      </footer>
     </div>
   );
 }
