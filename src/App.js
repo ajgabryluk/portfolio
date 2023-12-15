759d05e52567fe3fab3b803f014b43eef63fc6e4
DrFeelGoodz <aarong1@ptd.net>
2023-12-15 13:47:12 -0500

    Add header and footer components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
-import Home from './Components/home';
-import Projects from './Components/projects';
+import Home from './Components/Home';
+import Projects from './Components/Projects';
 
 function App() {
   return (
