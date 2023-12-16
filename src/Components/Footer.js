2ca76a12d60b7b735ad666b4544fc8b0c0c48765
DrFeelGoodz <aarong1@ptd.net>
2023-12-16 13:49:32 -0500

    Finished footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 import "../Styles/Footer.css"
 
 export default function Footer(){
+    const [hovering, setHovering] = useState(0)
     return(
         <footer>
-            <h1>THIS IS THE FOOTER</h1>
+            <p>Thanks for stopping by!</p>
+            <p class="callToAction">Please feel free to reach out to me via LinkedIn or Email.</p>
+            <div class="buttonBox">
+                <a href="https://www.linkedin.com/in/aarongabryluk/" 
+                    onMouseEnter={()=>{setHovering(1)}}
+                    onMouseLeave={()=>{setHovering(0)}}
+                >
+                    <LinkedInIcon fontSize='large' sx={{color:"#D90429", opacity: hovering === 0 || hovering === 1 ? 1 : 0.5}}/>
+                </a>
+                <a href="mailto:agabryluk3@gatech.edu"
+                    onMouseEnter={()=>{setHovering(2)}}
+                    onMouseLeave={()=>{setHovering(0)}}
+                >
+                    <EmailIcon fontSize='large' sx={{color:"#D90429", opacity: hovering === 0 || hovering === 2 ? 1 : 0.5}}/>
+                </a>
+            </div>
+            <p style={{opacity: 0.8}}>&#169; 2023 Aaron Gabryluk</p>
         </footer>
     )
 }
